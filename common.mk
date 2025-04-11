6a44a4a055eb5568eb5316fbea2b293c0841f606
Guille <guillermo.rabassa@paradoxcat.com>
2025-04-11 10:11:24 +0200

    Changes folder name and adds package to the common makefile.

--- common.mk
+++ common.mk
@@ -2,3 +2,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bootanimation-covesa.zip:system/media/bootanimation.zip
 
 BUILD_EMULATOR_CLUSTER_DISPLAY := true
+
+
+PRODUCT_PACKAGES += \
+	CustomCarProvision # Overrides CarProvision and ensures that the setup wizard is skipped
