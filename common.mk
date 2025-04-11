65cf0646ad662a91689d5c0d3bd446186b1a427d
Guille <guillermo.rabassa@paradoxcat.com>
2025-04-11 15:37:41 +0200

    Add CustomCarLauncher to common

--- common.mk
+++ common.mk
@@ -2,3 +2,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bootanimation-covesa.zip:system/media/bootanimation.zip
 
 BUILD_EMULATOR_CLUSTER_DISPLAY := true
+
+PRODUCT_PACKAGES += \
+	CustomCarLauncher
