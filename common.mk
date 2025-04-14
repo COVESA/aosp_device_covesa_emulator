b8ec22b5c2d08d625d2079c71f54f2d27f12d7d6
rabassa-pdx <guillermo.rabassa@paradoxcat.com>
2025-04-14 12:59:43 +0200

    Add information about the changes

--- common.mk
+++ common.mk
@@ -3,6 +3,6 @@ PRODUCT_COPY_FILES += \
 
 BUILD_EMULATOR_CLUSTER_DISPLAY := true
 
-
-PRODUCT_PACKAGES += \
-	CustomCarProvision
+# CarProvision is copied from https://cs.android.com/android/platform/superproject/+/android-12.0.0_r34:/packages/apps/Car/Provision
+# We modify the onCreate function to call finishSetup directly, avoiding the setup wizard screen
+PRODUCT_PACKAGES += CustomCarProvision
